485ea9e1bcb2d3b37385ffa943ba37e75880e3dd
jeffthemaximum <frey.maxim@gmail.com>
2016-02-13 17:09:56 -0500

    O(1) runtime

---
+++ 258-add-digits.py
@@ -0,0 +1,21 @@
+'''
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+'''
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0:
+            return 0
+        else:
+            return 1 + ((num - 1) % 9)
+
+sol = Solution()
+print(sol.addDigits(20))
