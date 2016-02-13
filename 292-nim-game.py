84bc4e2b7a8da9aef26b963c684947a87b446765
jeffthemaximum <frey.maxim@gmail.com>
2016-02-13 17:32:03 -0500

    solved

--- 292-nim-game.py
+++ 292-nim-game.py
@@ -1,5 +1,7 @@
 class Solution(object):
     def canWinNim(self, n):
+        if n < 4:
+            return True
         if n % 4 == 0:
             return False
         elif n % 5 == 0:
@@ -7,4 +9,4 @@ class Solution(object):
         elif n % 6 == 0:
             return True
         else:
-            return False
+            return True
