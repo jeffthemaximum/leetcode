58af877619defd55d5c04ce8c98bda6ef2a75d5f
jeffthemaximum <frey.maxim@gmail.com>
2016-02-14 20:32:50 -0500

    solved 217 with js

---
+++ algorithms/217-contains-duplicates.js
@@ -0,0 +1,20 @@
+// Given an array of integers, find if the array contains any duplicates. Your function should
+// return true if any value appears at least twice in the array, and it should return false if
+// every element is distinct.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    var count = [];
+    var len = nums.length;
+    for (var i = 0; i <= len; i++) {
+        if (count[nums[i]] === undefined) {
+            count[nums[i]] = 1;
+        } else {
+            return true;
+        }
+    }
+    return false;
+};
